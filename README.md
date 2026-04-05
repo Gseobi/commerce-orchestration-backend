d8d8c17aec51abe44a37a9281aac4eb63009f470
GSEOBI <wsx2386@naver.com>
2026-04-06 03:06:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# commerce-orchestration-backend
+commerce-orchestration-backend project
---
+++ src/test/java/io/github/gseobi/commerce/orchestration/CommerceOrchestrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gseobi.commerce.orchestration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceOrchestrationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
